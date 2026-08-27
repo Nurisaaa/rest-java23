cf51696ed047de71a8c27c1060f16385012398ba
Nurisa <nurisa.abdykalykova03@gmail.com>
2026-08-27 14:02:01 +0600

    add request param

--- src/main/java/peaksoft/school/restjava23/repository/LessonRepository.java
+++ src/main/java/peaksoft/school/restjava23/repository/LessonRepository.java
@@ -8,6 +8,6 @@ import peaksoft.school.restjava23.entities.Lesson;
 import java.util.List;
 
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
-    @Query("select new peaksoft.school.restjava23.dto.LessonResponse(l.id, l.name, l.dateOfPublication) from Lesson l where l.name ILIKE CONCAT('%', :name, '%')")
+    @Query("select new peaksoft.school.restjava23.dto.LessonResponse(l.id, l.name, l.dateOfPublication) from Lesson l where l.name ilike  CONCAT('%', :name, '%')")
     List<LessonResponse> findAllByName(String name);
 }
